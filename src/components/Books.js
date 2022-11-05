18c98288cf578067966320262b6ce4c6a0af0535
jfoyarzo <foyarzo621@gmail.com>
2022-11-04 22:32:39 -0300

    Add Books component

---
+++ src/components/Books.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Books = () => (
+  <div className="component-container" style={{ padding: '1em 3em' }}>
+    <div className="book-list" style={{ width: '60vw', height: '60vh', border: '1px solid black' }}>
+      <p>test</p>
+    </div>
+  </div>
+);
+
+export default Books;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import Navbar from './components/Navbar';
+import Books from './components/Books';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Books />} />
+      </Routes>
     </Router>
   </React.StrictMode>,
 );
