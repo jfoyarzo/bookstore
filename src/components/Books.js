c6fd1b06cfb4641a497cc7999f8ccb62ba52e5c4
jfoyarzo <foyarzo621@gmail.com>
2022-11-04 22:43:36 -0300

    Add Book component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Book from './Book';
 
 const Books = () => (
   <div className="component-container" style={{ padding: '1em 3em' }}>
-    <div className="book-list" style={{ width: '60vw', height: '60vh', border: '1px solid black' }}>
-      <p>test</p>
+    <div
+      className="book-list"
+      style={{
+        width: '60vw', height: '60vh', border: '1px solid black', padding: '1.5em',
+      }}
+    >
+      <Book title="The Hunger Games" author="Suzanne Collins" />
     </div>
   </div>
 );
