d3b89c330f5e2ea2fe98bf2daa04fc7be4ef0d0c
jfoyarzo <foyarzo621@gmail.com>
2022-11-08 22:20:23 -0300

    Add redux useDispatch to Book component and add function to handle click on the button to remove

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,7 +3,7 @@ const BOOK_REMOVED = 'bookstore/books/BOOK_REMOVED';
 
 const addBook = (payload) => ({ type: BOOK_ADDED, payload });
 
-const removeBook = (id) => ({ type: BOOK_REMOVED, payload: id });
+const removeBook = (id) => ({ type: BOOK_REMOVED, payload: { id } });
 
 const reducer = (state = [{ title: 'The Hunger Games', author: 'Suzanne Collins', id: '1' }, { title: 'Naked Lunch', author: 'William S. Burroughs', id: '2' }], action) => {
   switch (action.type) {
