db96c0f0b3d27b40cca920b71cb5de0c68629e2a
jfoyarzo <foyarzo621@gmail.com>
2022-11-08 22:34:08 -0300

    Add dispatch in Categories component for checkStatus action creator

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div className="categories-wrapper">
-    <button type="button">Check Status</button>
-  </div>
-);
+const Categories = () => {
+  const categories = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+  const handleClick = () => dispatch(checkStatus());
+  return (
+    <div className="categories-wrapper">
+      <p>{categories}</p>
+      <button type="button" onClick={handleClick}>Check Status</button>
+    </div>
+  );
+};
 
 export default Categories;
