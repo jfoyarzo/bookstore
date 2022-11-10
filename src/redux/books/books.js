135e72dfa46112c0aaae9c9fc93cab29798af86c
jfoyarzo <foyarzo621@gmail.com>
2022-11-10 14:50:58 -0300

    Add createAsyncThunk to book.js feature and add utils folder

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,8 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/Qo8LZ6Txlu6FMjkuPrCj/';
 
 const initialState = [
   { title: 'The Hunger Games', author: 'Suzanne Collins', id: '1' }, { title: 'Naked Lunch', author: 'William S. Burroughs', id: '2' },
 ];
+
+export const getBooks = createAsyncThunk(
+  'books/getBooks',
+  async () => {
+    const response = await axios(url);
+    return response.data;
+  },
+);
 
 const booksSlice = createSlice({
   name: 'books',
@@ -14,6 +25,12 @@ const booksSlice = createSlice({
       return [...state.filter((book) => book.id !== itemId)];
     },
   },
+  extraReducers: {
+    [getBooks.fulfilled]: (state, action) => {
+      const books = [...state, action.payload];
+      return books;
+    } 
+  }
 });
 
 export default booksSlice.reducer;
