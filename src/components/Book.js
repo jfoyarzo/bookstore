797fae47e6d75d7276e61c87e85ed441f4c4ea7c
jfoyarzo <foyarzo621@gmail.com>
2022-11-11 17:50:37 -0300

    Add style to book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,18 +2,27 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/books';
+import '../styles/book.css';
 
 const Book = (props) => {
-  const { title, author, id } = props;
+  const {
+    title, author, id, category,
+  } = props;
   const dispatch = useDispatch();
   const handleClick = () => {
     dispatch(removeBook(id));
   };
   return (
-    <div className="book-wrapper" style={{ display: 'flex', flexDirection: 'column' }}>
-      <h2>{title}</h2>
-      <p>{author}</p>
-      <button type="button" style={{ width: '5em' }} onClick={handleClick}>Remove</button>
+    <div className="book-wrapper">
+      <div className="text-wrapper">
+        <h3>{category}</h3>
+        <h2>{title}</h2>
+        <p>{author}</p>
+      </div>
+      <div className="button-wrapper">
+        <div className="vertical-line" />
+        <button type="button" onClick={handleClick}>Remove</button>
+      </div>
     </div>
   );
 };
--- src/pages/Books.js
+++ src/pages/Books.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import Book from '../components/Book';
 import Form from '../components/Form';
 import { getBooks } from '../redux/books/books';
+import '../styles/books.css';
 
 const Books = () => {
   const books = useSelector((state) => state.books);
@@ -13,17 +14,19 @@ const Books = () => {
     dispatch(getBooks());
   }, []);
   return (
-    <div className="component-container" style={{ padding: '1em 3em' }}>
-      <div
-        className="book-list"
-        style={{
-          width: '60vw', height: '60vh', border: '1px solid black', padding: '1.5em',
-        }}
-      >
+    <div className="component-container">
+      <div className="book-list">
         { books.map((book) => (
-          <Book key={book.item_id} title={book.title} author={book.author} id={book.item_id} />
+          <Book
+            key={book.item_id}
+            title={book.title}
+            author={book.author}
+            id={book.item_id}
+            category={book.category}
+          />
         ))}
       </div>
+      <hr />
       <Form />
     </div>
   );
